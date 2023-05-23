7b084ec395f88ffaccaa2e1a3d5f8f2502452f6a
fiber21 <senjuhanif@gmail.com>
2023-05-23 12:19:35 +0700

    memperbaiki fungsi hitung keliling

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function hitungKeliling() {
     const sisiKeliling = document.getElementById("sisi-keliling").value;
-    const rumus = document.getElementById("perhitungan-keliling");
-    const hasil = document.getElementById("hasil-keliling");
+    const hasil = document.getElementById("inner");
+    // const hasil = document.getElementById("hasil-keliling");
     // console.log(sisiLuas);
-    rumus.innerHTML = "K = 4 x " + sisiKeliling;
-    hasil.innerHTML = 4 * sisiKeliling;
+    // rumus.innerHTML = "K = 4 x " + sisiKeliling;
+    // hasil.innerHTML = 4 * sisiKeliling;
+    hasil.innerHTML = "<p class='result-keliling' id='rumus-keliling'>K = 4 x S</p> <p class='result-keliling' id='perhitungan-keliling'>K = 4 x " + sisiKeliling + " cm</p> <p class='result-keliling' id='hasil-keliling'>"+4 * sisiKeliling+" cm</p>";
 }
 
