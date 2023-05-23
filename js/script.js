f17d83427422cdfceecd1bfc433f223139457f51
fiber21 <senjuhanif@gmail.com>
2023-05-23 12:47:38 +0700

    membuat fungsi luas dan membersihkan komen

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
+// 2) lalu diaplikasikan untuk fungsi luas
+function hitungLuas() {
+    const sisiLuas = document.getElementById("sisi-luas").value;
+    const hasil = document.getElementById("inner-luas");
+
+    hasil.innerHTML = "<p class='result-luas' id='rumus-luas'>L = S x S</p> <p class='result-luas' id='perhitungan-luas'>L = "+ sisiLuas +" x " + sisiLuas + " cm</p> <p class='result-luas' id='hasil-luas'>L = "+ sisiLuas * sisiLuas+" cm</p>";
+}
+// 2) lalu diaplikasikan untuk fungsi luas
+// 1)fungsi ini dibuat terlebih dahulu
 function hitungKeliling() {
     const sisiKeliling = document.getElementById("sisi-keliling").value;
-    const hasil = document.getElementById("inner");
-    // const hasil = document.getElementById("hasil-keliling");
-    // console.log(sisiLuas);
-    // rumus.innerHTML = "K = 4 x " + sisiKeliling;
-    // hasil.innerHTML = 4 * sisiKeliling;
-    hasil.innerHTML = "<p class='result-keliling' id='rumus-keliling'>K = 4 x S</p> <p class='result-keliling' id='perhitungan-keliling'>K = 4 x " + sisiKeliling + " cm</p> <p class='result-keliling' id='hasil-keliling'>"+4 * sisiKeliling+" cm</p>";
+    const hasil = document.getElementById("inner-keliling");
+    
+    hasil.innerHTML = "<p class='result-keliling' id='rumus-keliling'>K = 4 x S</p> <p class='result-keliling' id='perhitungan-keliling'>K = 4 x " + sisiKeliling + " cm</p> <p class='result-keliling' id='hasil-keliling'> K = "+4 * sisiKeliling+" cm</p>";
 }
-
+// 1)fungsi ini dibuat terlebih dahulu
