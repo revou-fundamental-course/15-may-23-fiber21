db20ea00be2cd3fe76f15c91350b9e3cacbd3c62
fiber21 <senjuhanif@gmail.com>
2023-05-25 15:55:12 +0700

    mengganti nama div inner menjadi output

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // 2) lalu diaplikasikan untuk fungsi luas
 function hitungLuas() {
     const sisiLuas = document.getElementById("sisi-luas").value;
-    const hasil = document.getElementById("inner-luas");
+    const hasil = document.getElementById("output-luas");
 
     hasil.innerHTML = "<p class='result-luas' id='rumus-luas'>L = S x S</p> <p class='result-luas' id='perhitungan-luas'>L = "+ sisiLuas +" x " + sisiLuas + " cm</p> <p class='result-luas' id='hasil-luas'>L = "+ sisiLuas * sisiLuas+" cm</p>";
 }
@@ -9,19 +9,19 @@ function hitungLuas() {
 // 1)fungsi ini dibuat terlebih dahulu
 function hitungKeliling() {
     const sisiKeliling = document.getElementById("sisi-keliling").value;
-    const hasil = document.getElementById("inner-keliling");
+    const hasil = document.getElementById("output-keliling");
     
     hasil.innerHTML = "<p class='result-keliling' id='rumus-keliling'>K = 4 x S</p> <p class='result-keliling' id='perhitungan-keliling'>K = 4 x " + sisiKeliling + " cm</p> <p class='result-keliling' id='hasil-keliling'> K = "+4 * sisiKeliling+" cm</p>";
 }
 // 1)fungsi ini dibuat terlebih dahulu
 // 3) membuat fungsi reset
 function resetLuas() {
-    const hasil = document.getElementById("inner-luas");
+    const hasil = document.getElementById("output-luas");
     hasil.innerHTML = "";
 }
 
 function resetKeliling() {
-    const hasil = document.getElementById("inner-keliling");
+    const hasil = document.getElementById("output-keliling");
     hasil.innerHTML = "";
 }
 // 3) membuat fungsi reset
